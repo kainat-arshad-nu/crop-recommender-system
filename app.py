c915ec439f6050bedc1c97ec01825a70df425f31
Syed-Adnan-Tariq <98732460+kainat-arshad-nu@users.noreply.github.com>
2022-04-17 16:51:22 +0500

    conditional

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def predict():
     o1 = single
     o2 = "Confidence: {}".format(probablity*100)
         
-    return render_template('home.html', output1=o1, output2=o2, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'],query4 = request.form['query4'],query5 = request.form['query5'], query6 = request.form['query6'])
-    
-app.run()
+    return render_template('index.html', output1=o1, output2=o2, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'],query4 = request.form['query4'],query5 = request.form['query5'], query6 = request.form['query6'])
+
+if __name__ == "__main__":
+    app.run()
